39159b6e6e0c996ee75e635fd65ddd19145c151b
Riamse <andrewwang43@gmail.com>
2014-08-14 19:40:54 -0700

    Assimilation to Python

--- apple.py
+++ apple.py
@@ -2,12 +2,12 @@ from random import randint
 
 class Apple:
     def __init__(self, width, height, snake_pos):
-        self.pos = random_pos(width, height, snake_pos)
-        self.gold = True if randint(0, 100) < 3 else False
+        self.pos = random_pos(width, height, snake_pos) # where is this defined
+        self.gold = (randint(0, 100) < 3)
 
     def random_pos(width, height, snake_pos):
-        loc = (randint(0, width), randint(0, height))
-        while (loc in snake_pos):
-            loc = (randint(0, width), randint(0, height))
+        loc = randint(0, width), randint(0, height)
+        while loc in snake_pos:
+            loc = randint(0, width), randint(0, height)
         return loc
 
