2ed0516e6bae37c7906a9240208d00dbbe348295
kikiotsuka <mitsuru.otsuka@gmail.com>
2014-08-14 18:52:41 -0700

    added more code

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import curses
+from time import sleep
 
-crs = None
+window = None
+snake = None
+apple = None
+WIDTH = 80
+HEIGHT = 24
 
 def __main__():
-	global crs
-	crs = curses.initscr()
-	curses.noecho()
-	curses.cbreak()
-	crs.keypad(1)
-	quit()
+	try:
+		global window, snake, apple
+		window = curses.initscr()
+		curses.noecho()
+		curses.cbreak()
+		window.keypad(1)
+		curses.newwin(10, 5, 10, 10)
+		#print(window.getmaxyx())
+		#window.clear()
+		#curses.resizeterm(HEIGHT, WIDTH) <- doesnt work
+		#window.refresh()
+		#sleep(2)
+
+		snake = Snake(WIDTH, HEIGHT)
+		apple = Apple(WIDTH, HEIGHT, snake.pos)
+
+		while (snake.alive):
+			pass
+
+		quit()
+	except:
+		print('FATAL ERROR')
+		print('Sleeping for 5 seconds so you can read this')
+		sleep(5)
+		quit()
 
 def quit():
-	global crs
+	global window
 	curses.nocbreak()
-	crs.keypad(0)
+	window.keypad(0)
 	curses.echo()
 	curses.endwin()
 
 if __name__ == '__main__':
 	__main__()
+
+"""
+
+TODO
+- resize window? (for terminals with different screen sizes)
+- figure out how curses freaking works
+- game loop
+- how to show score?
+	- maybe have number on screen that moves around if your snake gets close to it or something
+- portability?
+
+"""
