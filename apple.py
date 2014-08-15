e68efa81c9f5c715ae50846ccbc5b2794ecb1c19
Riamse <andrewwang43@gmail.com>
2014-08-14 20:39:05 -0700

    Minor typo fix numero dos

--- apple.py
+++ apple.py
@@ -5,7 +5,7 @@ class Apple:
         self.pos = self.random_pos(width, height, snake_pos)
         self.gold = (randint(0, 100) < 3)
 
-    def random_pos(width, height, snake_pos):
+    def random_pos(self, width, height, snake_pos):
         loc = randint(0, width), randint(0, height)
         while loc in snake_pos:
             loc = randint(0, width), randint(0, height)
