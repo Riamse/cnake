21fc5239bf000d9128677b12e39c766e3b024442
kikiotsuka <mitsuru.otsuka@gmail.com>
2014-08-16 11:58:08 -0700

    updated colors, fixed snake body, updated refresh rate

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ from time import sleep
 from snake import Snake
 from apple import Apple
 
+class Color():
+    BLACK = 1 #snake color
+    GREEN = 2 #apple color
+    GOLD = 3 #apple golden color
+
 dir_dict = {curses.KEY_UP: "up", curses.KEY_DOWN: "down", curses.KEY_LEFT: "left", curses.KEY_RIGHT: "right"}
 WIDTH, HEIGHT = 80, 24#os.get_terminal_size()
 
 def main(window):
     curses.use_default_colors()
-    for i in range(0, curses.COLORS):
-        curses.init_pair(i, i, -1)
+    curses.init_pair(Color.BLACK, curses.COLOR_BLACK, curses.COLOR_BLACK)
+    curses.init_pair(Color.GREEN, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(Color.GOLD, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    #for i in range(0, curses.COLORS):
+        #curses.init_pair(i, i, -1)
     curses.curs_set(0)  # invisible cursor
     window.nodelay(1)
     window.clear()
@@ -23,15 +31,16 @@ def main(window):
     snake = Snake(WIDTH, HEIGHT)
 
     while snake.alive:
-        apple_colour = curses.color_pair(3) if snake.apple.gold   or 1 else 0
+        apple_colour = curses.color_pair(Color.GOLD) if snake.apple.gold else curses.color_pair(Color.GREEN)
         window.addch(snake.apple.y, snake.apple.x, b"A", apple_colour)
         window.move(0, 0)
-        for x, y in snake.body:
-            window.addch(y, x, b"X")
+        window.addch(snake.body[-1][1], snake.body[-1][0], b"X")
+        for x, y in snake.body[:-1]:
+            window.addch(y, x, b"O")
             window.refresh()
             #sleep(0.01)  # curses is retarded
         window.refresh()
-        sleep(0.1)
+        sleep(0.05)
         window.clear()
         ch = window.getch()
         if ch == ord("q"):
@@ -48,16 +57,3 @@ if __name__ == '__main__':
     except Exception:
         traceback.print_exc()
         raise SystemExit(0)
-
-"""
-
-TODO
-- resize window? (for terminals with different screen sizes)
-- figure out how curses freaking works
-- game loop
-- how to show score?
-    - maybe have number on screen that moves around if your snake gets close to it or something
-- portability?
-
-"""
-
