d1ec491bb48ec59f5c625270163b96e8f07a73b9
Mitsuru <mitsuru.otsuka@gmail.com>
2014-08-22 10:28:31 -0700

    implementing path finding

--- ai.py
+++ ai.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from snake import Snake
+from time import sleep
 
 class Ai(Snake):
     def __init__(self, width, height):
@@ -8,6 +9,7 @@ class Ai(Snake):
         grid = [[0 for i in range(width)] for i in range(height - 5)]
         turn_points = [] #tuple -> ((x, y), new_direction)
         apple_loc = self.apple.pos
+        self.generate_path()
 
     def move(self):
         if len(turn_points) > 0:
@@ -24,5 +26,21 @@ class Ai(Snake):
     #a* implementation of path finding
     def generate_path(self):
         location_queue = [self.position()]
+        #instead of using priority queue, use regular list and sort it
+        #with key function that returns distance to destinatetion
         while len(location_queue > 0) or self.position() != self.apple.pos:
-            pass #implement priority queue heuristic algo
+            location_queue.append((self.x + 1, self.y))
+            location_queue.append((self.x - 1, self.y))
+            location_queue.append((self.x, self.y + 1))
+            location_queue.append((self.x, self.y - 1))
+            location_queue.sort(key=self.dist)
+            for blah in location_queue:
+                print(blah)
+                sleep(3)
+            return
+
+    def dist(self, val):
+        dx = self.apple.x - val[0]
+        dy = self.apple.y - val[1]
+        to_square = dx * dx + dy * dy
+        return to_square ** 2
